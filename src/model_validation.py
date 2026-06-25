16a7ecdc68f4f9a747aa4553e052fe4481f87e8d
tarunsaxena2 <saxenatarun272@gmail.com>
2026-06-25 12:23:28 +0530

    fix: sanitize feature names for lightgbm compatibility (fixes #14)

--- src/model_validation.py
+++ src/model_validation.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import StratifiedKFold
 from imblearn.over_sampling import SMOTE
 from lightgbm import LGBMClassifier
 from imblearn.pipeline import Pipeline
+from sklearn.model_selection import cross_validate
 
 df = pd.read_csv("data/fused_dataset.csv")
 
@@ -13,14 +14,38 @@ print("Shape:", df.shape)
 
 target_col = "Machine failure"
 
-X = df.drop(columns=[target_col])
+X = df.drop(
+    columns=[
+        target_col,
+        "Product ID"
+    ]
+)
+
+X = pd.get_dummies(
+    X,
+    columns=["Type"],
+    drop_first=True
+)
 
 y = df[target_col]
+# Clean column names for LightGBM
 
+X.columns = (
+    X.columns
+    .str.replace("[", "", regex=False)
+    .str.replace("]", "", regex=False)
+    .str.replace("(", "", regex=False)
+    .str.replace(")", "", regex=False)
+    .str.replace(" ", "_", regex=False)
+    .str.replace("/", "_", regex=False)
+)
+print("\nSample Feature Names:")
+
+for col in X.columns[:10]:
+    print(col)
 print("\nFeature Shape:", X.shape)
 
 print("Target Shape:", y.shape)
-
 cv = StratifiedKFold(
     n_splits=5,
     shuffle=True,
@@ -54,3 +79,20 @@ print("\nPipeline Created Successfully")
 print("Step 1: SMOTE")
 
 print("Step 2: LightGBM")
+
+print("\nRunning Cross Validation...\n")
+
+scores = cross_validate(
+    pipeline,
+    X,
+    y,
+    cv=cv,
+    scoring=[
+        "f1_macro",
+        "precision_macro",
+        "recall_macro"
+    ],
+    n_jobs=-1
+)
+
+print("Cross Validation Completed")
